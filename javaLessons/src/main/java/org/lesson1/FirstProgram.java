9062e8d5218de51360001f971d2c9e4080f11df9
IlyaShlimakov <raccoonshlem@gmail.com>
2022-02-06 23:40:58 +0300

    Lesson_1

---
+++ javaLessons/src/main/java/org/lesson1/FirstProgram.java
@@ -0,0 +1,11 @@
+package org.lesson1;
+
+import java.util.Scanner;
+
+public class FirstProgram {
+    public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        System.out.println(number);
+    }
+}
