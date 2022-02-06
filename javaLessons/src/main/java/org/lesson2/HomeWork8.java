01319a441c55d645f6220b9515f5a51864ac2389
IlyaShlimakov <raccoonshlem@gmail.com>
2022-02-06 23:58:11 +0300

    Lesson_2

---
+++ javaLessons/src/main/java/org/lesson2/HomeWork8.java
@@ -0,0 +1,16 @@
+package org.lesson2;
+//Task8
+public class HomeWork8 {
+    public static void main(String[] args) {
+        int[][] array = new int[15][15];
+        for (int i = 0; i < array.length; i++) {
+            System.out.println();
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = i == j ?  1 : (int) (10 + Math.random() * 20);
+                array[i][j] = i+j+1 == array.length ? 1 : array[i][j];
+                System.out.print(array[i][j]+" ");
+            }
+
+        }
+    }
+}
