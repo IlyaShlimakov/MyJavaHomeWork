d5f51a5bff330af9fb0d329bd6c971668d2a4620
IlyaShlimakov <raccoonshlem@gmail.com>
2022-03-07 20:26:51 +0300

    Lesson_8

---
+++ javaLessons/src/main/java/org/lesson8/Task8.java
@@ -0,0 +1,31 @@
+package org.lesson8;
+
+
+import java.util.*;
+
+public class Task8 {
+    public static void main(String[] args) {
+        String[] words = {"dog", "cat", "pet", "lock", "mock", "lock", "fork", "mock", "golf", "lock"};
+
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+        System.out.println(map);
+        System.out.println("");
+
+        Set<String> uniq = new HashSet<>(Arrays.asList(words));
+        System.out.println(uniq);
+        System.out.println("");
+
+        Phonebook pb = new Phonebook();
+
+        pb.add("Ivanov", "143");
+        pb.add("Petrov", "446");
+        pb.add("Petrov", "765");
+
+        System.out.println(pb.get("Petrov"));
+    }
+}
+
+
