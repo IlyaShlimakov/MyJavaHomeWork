f310fd99c973c6eb1a7938946402dec96a622c67
IlyaShlimakov <raccoonshlem@gmail.com>
2022-04-14 12:25:48 +0300

    Example1

---
+++ javaLessons/src/main/java/Example/First2.java
@@ -0,0 +1,15 @@
+package Example;
+import java.util.Scanner;
+public class First2 {
+    public static void main(String[] agrs) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите имя\n");
+        String checkName = in.nextLine();
+        String comparingName = "Вячеслав";
+        if (checkName.equalsIgnoreCase(comparingName)) {
+            System.out.print("Привет, Вячеслав\n");
+        } else {
+            System.out.print("Нет такого имени\n");
+        }
+    }
+}
